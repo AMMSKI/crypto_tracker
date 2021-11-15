069165567681b596caf71ce6e7ce8e30db1e3000
Alan Mamulski <alanmamulski@gmail.com>
2021-11-15 00:32:13 -0700

    react-router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 import './App.css';
 import Feed from './Feed';
 
 function App() {
   return (
     <div className="App">
-      <Feed />
+      <Link to='/'>Feed</Link>
+      <Routes>
+        <React.Fragment>
+        <Route path='/' element={<Feed/>} />
+        </React.Fragment>
+      </Routes>
     </div>
   );
 }
