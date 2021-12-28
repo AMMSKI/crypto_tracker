19fabdebd51ce2b905a7530865dd09941593023a
Alan Mamulski <alanmamulski@gmail.com>
2021-12-27 22:44:41 -0700

    hero image section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import { Routes, Route } from "react-router-dom";
 import './App.css';
+import Hero from './Components/Hero';
 import Feed from './Feed';
 
 function App() {
   return (
     <div className="App">
+      <Hero />
       <Routes>
         <Fragment>
         <Route path='/' element={<Feed/>} />
---
+++ src/Components/Hero.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../StyleSheets/hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero_container'>
+      Hero
+    </div>
+  )
+}
+
+export default Hero
